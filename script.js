26e3f162ad8196006bfbc8d910a582149a8b6160
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-24 13:22:09 -0800

    Added coloring, and handling for grid size.

--- script.js
+++ script.js
@@ -4,25 +4,51 @@ main.id = "main";
 sidepanel = document.createElement("div");
 sidepanel.id = "sidepanel";
 main.appendChild(sidepanel);
+
+sizer = document.createElement("div");
+sizer.id = "sizer";
+sizerinput = document.createElement("input");
+sizerinput.type = "number";
+sizerinput.max = "100";
+sizerinput.value = "16";
+sizerinput.id = "sizerinput";
+sizer.appendChild(sizerinput);
+sidepanel.appendChild(sizer);
+
+document.addEventListener("change", function(e) {
+    if (e.target.id == sizerinput.id) {
+        container.innerHTML = "";
+        grid();
+    }
+})
+
 container = document.createElement("div");
 container.id = "container";
-for (let i = 0; i <= 16; i++) {
-    var vertical = document.createElement("div");
-    vertical.id = i;
-    vertical.className = "vertical";
-    for (let j = 0; j <= 16; j++) {
-        var horizontal = document.createElement("div");
-        horizontal.id = i + "-" + j
-        horizontal.className = "horizontal";
-        var p = document.createElement("p");
-        horizontal.appendChild(p);
-        vertical.appendChild(horizontal);
+function grid() {
+    console.log(sizerinput.value);
+    if (sizerinput.value <= 100) {
+        for (let i = 0; i <= sizerinput.value; i++) {
+            var vertical = document.createElement("div");
+            vertical.id = i;
+            vertical.className = "vertical";
+            for (let j = 0; j <= sizerinput.value; j++) {
+                var horizontal = document.createElement("div");
+                horizontal.id = i + "-" + j
+                horizontal.className = "horizontal";
+                var p = document.createElement("p");
+                horizontal.appendChild(p);
+                vertical.appendChild(horizontal);
+            }
+            container.appendChild(vertical);
+        }
+        main.appendChild(container);
+        body.appendChild(main);
     }
-    container.appendChild(vertical);
+    else {
+        alert("Invalid Value");
+    }
 }
-main.appendChild(container);
-body.appendChild(main);
-
+grid();
 document.addEventListener("mousemove", function(e) {
     if (e.target.className == "horizontal") {
         console.log("hovered");
