87c8856bc4cca3202933b57dce322647e4c498f3
mrmasterb4 <140006398+mrmasterb4@users.noreply.github.com>
2024-01-24 14:04:37 -0800

    Added RGB random colors on hover

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+// Main elements
 body = document.querySelector("body");
 main = document.createElement("div");
 main.id = "main";
+
+// Sidepanel
 sidepanel = document.createElement("div");
 sidepanel.id = "sidepanel";
+sideheader = document.createElement("p");
+sideheader.textContent = "MENU";
+sidepanel.appendChild(sideheader);
 main.appendChild(sidepanel);
 
+// Rainbow Handling
+rainbowdiv = document.createElement("div");
+rainbowdiv.id = "rainbowdiv";
+rainbowtext = document.createElement("p");
+rainbowtext.textContent = "Toggle Rainbow";
+rainbowdiv.appendChild(rainbowtext);
+rainbowvalue = document.createElement("input");
+rainbowvalue.id = "rainbow";
+rainbowvalue.type = "checkbox";
+rainbowdiv.appendChild(rainbowvalue);
+sidepanel.appendChild(rainbowdiv);
+
+// Grid Sizing Handler
 sizer = document.createElement("div");
 sizer.id = "sizer";
 sizerinput = document.createElement("input");
@@ -12,16 +31,35 @@ sizerinput.type = "number";
 sizerinput.max = "100";
 sizerinput.value = "16";
 sizerinput.id = "sizerinput";
+sizertext = document.createElement("p");
+sizertext.textContent = "Grid Size";
+sizer.appendChild(sizertext);
 sizer.appendChild(sizerinput);
 sidepanel.appendChild(sizer);
 
+// Detect change in sizer value
+var rainbow = 0;
 document.addEventListener("change", function(e) {
     if (e.target.id == sizerinput.id) {
         container.innerHTML = "";
         grid();
     }
+    else if (e.target.id == rainbowvalue.id) {
+        if (rainbow == 0) {
+            rainbow = 1;
+            container.innerHTML = "";
+            grid();
+        }
+        else {
+            rainbow = 0;
+            container.innerHTML = "";
+            grid();
+        }
+    }
 })
 
+
+// Grid function.
 container = document.createElement("div");
 container.id = "container";
 function grid() {
@@ -49,8 +87,21 @@ function grid() {
     }
 }
 grid();
+function getRGB() {
+    var R = Math.floor(Math.random() * (255 - 0 + 1) + 0)
+    var G = Math.floor(Math.random() * (255 - 0 + 1) + 0)
+    var B = Math.floor(Math.random() * (255 - 0 + 1) + 0)
+    return "rgb(" + R + "," + G + "," + B + ")";
+}
+console.log("value is", rainbowvalue.value);
+// Detect move, and change color.
 document.addEventListener("mousemove", function(e) {
-    if (e.target.className == "horizontal") {
+    if (e.target.className == "horizontal" && rainbow == 1) {
+        console.log(rainbowvalue.value);
+        console.log("rainbow");
+        e.target.style.backgroundColor = getRGB();
+    }
+    else if (e.target.className == "horizontal") {
         console.log("hovered");
         e.target.style.backgroundColor = "white";
     }
